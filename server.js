8004395db72a12222ed9e1c1ca5cd2f7f2bdeb71
bbtechpro <billy.barber@gmail.com>
2026-05-25 17:56:25 -0400

    Updated path for routes to include the index.js file.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./routes/index');
 
  
 const app = express();
