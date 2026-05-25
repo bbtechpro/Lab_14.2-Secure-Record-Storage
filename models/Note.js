4453e21c4fce046ee1ec556e8a6200193c0eb927
bbtechpro <billy.barber@gmail.com>
2026-05-25 14:42:16 -0400

    Update the Note Model: Add a new field to the Note schema (models/Note.js). This field should be named user (or owner) and should store the ObjectId of the user who created the note. It should be a required reference to the User model.

--- models/Note.js
+++ models/Note.js
@@ -15,6 +15,11 @@ const noteSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
  
 const Note = model('Note', noteSchema);
