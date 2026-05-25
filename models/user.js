de43d99a843c043f8a668be5c1f4d760694a539e
bbtechpro <billy.barber@gmail.com>
2026-05-25 19:46:34 -0400

    tweaked model

--- models/user.js
+++ models/user.js
@@ -22,13 +22,11 @@ const userSchema = new Schema({
 });
  
 // hash user password
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
- 
-  next();
 });
  
 // custom method to compare and validate password for logging in
@@ -37,5 +35,5 @@ userSchema.methods.isCorrectPassword = async function (password) {
 };
  
 const User = model('User', userSchema);
- 
-module.exports = User;
+
+module.exports = { User };
