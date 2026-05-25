7275a3ed0e01146d735f338d6b861b350034ded0
bbtechpro <billy.barber@gmail.com>
2026-05-25 19:48:42 -0400

    Modified middleware function

--- utils/auth.js
+++ utils/auth.js
@@ -5,8 +5,8 @@ const expiration = '2h';
  
 module.exports = {
   authMiddleware: function (req, res, next) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
- 
+    let token = (req.body && req.body.token) || req.query.token || req.headers.authorization;
+
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
